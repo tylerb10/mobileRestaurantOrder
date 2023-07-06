574b24339b1f44ee1a64ccfcb3bb41745cd7d53a
tylerb10 <97489607+tylerb10@users.noreply.github.com>
2023-07-05 19:46:41 -0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const foodData = [
+    {
+        pic: `images/pizza.png`,
+        itemName: `Pizza`,
+        ingredients: `pepperoni, cheese, sausage`,
+        price: 14,
+        isInCart: false,
+        uuid: `cbfc87fe-ca7a-4340-8922-40e6e04f531d`
+    },
+    {
+        pic: `images/burger.png`,
+        itemName: `Hamburger`,
+        ingredients: `beef, lettuce, cheese optional`,
+        price: 9,
+        isInCart: false,
+        uuid: `20f17a4c-e745-4456-bc4f-fe95608844b6`
+    },
+    {
+        pic: `images/beer.png`,
+        itemName: `Beer`,
+        ingredients: `grain, hops, yeast, water`,
+        price: 12,
+        isInCart: false,
+        uuid: `cb8da508-a326-4e81-aaf4-be24cfa840c2`
+    }
+]
---
+++ index.js
@@ -0,0 +1,67 @@
+import { foodData } from "./data.js";
+
+const orderItems = []
+
+
+function getMenuList(){
+    let menuList = ``
+
+    foodData.forEach(function(food){
+
+        // let isAddedClass = ''
+
+        // if (food.isInCart){
+        //     isAddedClass = 'inCart'
+        // }
+    
+        menuList += `
+            <div class="item"> 
+                <div class="item-img">
+                    <img src="${food.pic}" class="item-pic">
+                </div>
+                <div class="item-text">
+                    <p class="item-name">${food.itemName}</p>
+                    <i class="item-ingredients">${food.ingredients}</i>
+                    <p class="item-price">$ ${food.price}</p>
+                </div>
+                <div class="item-right">
+                    <button class="add-btn"></button>
+                </div>
+            </div>    
+            `
+        })
+        return menuList
+        }
+
+function render(){
+    document.getElementById('feed').innerHTML = getMenuList()
+}
+
+
+
+
+
+
+function getOrderList() {
+    let orderHtml = ''
+
+    if (orderItems.length > 0) {
+       orderItems.forEach(function(item){
+        orderHtml +=`
+            <div class="order-items">
+                <div class="item-text">
+                    <p class="item-name">${item.itemName}</p>
+                    <button class="remove-btn">REMOVE</button>
+                </div>
+                <div class="item-right">
+                    <p class="item-price">$ ${item.price}</p>
+                </div>
+            </div>
+        `
+       })
+    }
+}
+
+
+
+render()
